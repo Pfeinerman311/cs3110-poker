b1e5dd39475e7db8e1504fd162e6664076f45ca7
Parker Feinerman <pjf73@cornell.edu>
2020-10-19 18:42:29 -0400

    Added card types to poker.

---
+++ poker.ml
@@ -0,0 +1,6 @@
+type rank = One | Two | Three | Four | Five | Six | Seven | Eight | Nine
+          | Ten | Jack | Queen | King | Ace
+
+type suit = Clubs | Diamonds | Hearts | Spades
+
+type card = rank * suit
