8dc3fe5557163b02b6c159f5a1541db47aa6bc1e
Parker Feinerman <pjf73@cornell.edu>
2020-10-19 19:06:54 -0400

    Added deck.

--- poker.ml
+++ poker.ml
@@ -4,3 +4,16 @@ type rank = Two | Three | Four | Five | Six | Seven | Eight | Nine
 type suit = Clubs | Diamonds | Hearts | Spades
 
 type card = rank * suit
+
+let ranks =
+  [Two; Three; Four; Five; Six; Seven; Eight; Nine; Ten; 
+   Jack; Queen; King; Ace]
+
+let rec deck_builder ranks deck =
+  match ranks with
+  | [] -> deck
+  | h::t -> deck_builder t deck@[(h, Clubs); (h, Diamonds); 
+                                 (h, Hearts); (h, Spades)]
+
+let deck = 
+  deck_builder ranks []
