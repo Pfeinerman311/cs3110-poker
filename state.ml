22e1384f9cfd5c19bf3be4b06afe09131387b659
Dean Edward Alvarez <dea79@cornell.edu>
2020-10-24 12:38:13 -0400

    finish state features. TODO: end_round

--- state.ml
+++ state.ml
@@ -5,6 +5,7 @@ type t = {
   pot: int;
   community_cards: Poker.card list;
   call_cost: int;
+  deck : Poker.card list;
 }
 
 type result = Legal of t | Illegal
@@ -13,7 +14,7 @@ let init_state players =
   if 1< List.length players && List.length players < 10 then
     Legal {players = players; big_blind= List.hd players;
            player_to_act = List.hd (List.tl players); pot = 0;
-           community_cards = []; call_cost=0}
+           community_cards = []; call_cost=0; deck=[]}
   else Illegal
 
 let current_player state = 
@@ -50,19 +51,48 @@ let call state player =
     Legal {state with players =  new_players; pot = state.pot + state.call_cost}
 
 let fold state player = 
-  failwith "Unimplemented"
+  let new_players = List.map (fun x -> 
+      if Poker.get_ID x = Poker.get_ID player then Poker.set_inactive x
+      else x) state.players in
+  {state with players = new_players}
+
+let rec first_n_helper list num stop acc = 
+  match list with
+  | [] -> acc,list
+  | h::t -> if num = stop then acc,list 
+    else first_n_helper t (num+1) stop (h::acc)
+
+
+let first_n list n = 
+  first_n_helper list 0 n []
+
+let rec deal_helper players deck acc = 
+  match players with
+  | [] -> acc
+  | h::t -> 
+    let hole_cards , remaining_deck = first_n deck 2 in
+    let new_player = Poker.set_hole_cards h hole_cards in
+    deal_helper t (List.tl remaining_deck) (new_player::acc)
 
 let deal state =
-  failwith "Unimplemented"
+  let deck = Poker.get_shuffled_deck () in
+  let rev_player = List.rev state.players in
+  {state with players= deal_helper rev_player deck [] }
+
 
 let flop state = 
-  failwith "Unimplemented"
+  let flop_cards, remaining_deck = first_n state.deck 3 in
+  {state with community_cards=flop_cards; deck = remaining_deck}
 
 let turn state = 
-  failwith "Unimplemented"
+  let turn_card, remaining_deck = first_n state.deck 1 in
+  {state with community_cards= List.concat [turn_card;state.community_cards];
+              deck = remaining_deck;}
 
 let river state = 
-  failwith "Unimplemented"
+  let river_card, remaining_deck = first_n state.deck 1 in
+  {state with community_cards= List.concat [river_card;state.community_cards];
+              deck = remaining_deck;}
 
 let end_round state = 
   failwith "Unimplemented"
