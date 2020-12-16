8e20ae8f755d5449dfffbd234172230bcba411a9
Dean Alvarez <dea79@cornell.edu>
2020-12-16 00:16:06 -0500

    start some calculations

--- myBot.ml
+++ myBot.ml
@@ -7,8 +7,34 @@ module Make = functor (I : BotInfo) -> struct
   let get_cards_that_improve_hand hand stage = 
     failwith "Unimplemented"
 
-  let calculate_prob_of_drawing_cards cards state = 
-    failwith "Unimplemented"
+  let calculate_prob_of_drawing_cards cards_list state = 
+    let stage = State.get_stage state in
+    let get_prob cards = 
+      match List.length cards with
+      | 1 -> begin 
+          match stage with
+          | Deal -> 0.1
+          | Flop -> 2. /. 47.
+          | Turn -> 1. /. 46.
+          | _ -> failwith "this should be unreachable"
+        end
+      | 2 -> begin 
+          match stage with
+          | Deal -> 2. /. 245.
+          | Flop -> 1. /. 1081.
+          | Turn -> 0.
+          | _ -> failwith "this should be unreachable"
+        end
+      | 3 -> begin 
+          match stage with
+          | Deal -> 1. /. 1960.
+          | Flop -> 0.
+          | Turn -> 0.
+          | _ -> failwith "this should be unreachable"
+        end
+      | _ -> failwith "Unimplemented"
+    in 
+    List.map get_prob cards_list
 
   let calculate_probabilities_of_hands_winning hands state = 
     failwith "Unimplemented"
