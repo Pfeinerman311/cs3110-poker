fdd508ada2c80a61f17ff6eb8beb491e34e86133
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2020-12-20 17:35:45 -0500

    - completed mli for main

--- main.mli
+++ main.mli
@@ -1,14 +1,14 @@
 (** The main interface of the poker game. *)
 
 (** [prompt_user st] parses a user's input into a command, and performs the
-    command by applying a command to the state [st] in order to print relevant 
-    information to the user. *)
+    command by applying a it to the state [st] in order to advance the game
+    and show relevant information to the user. *)
 val prompt_user_command : State.t -> State.t
 
-(** [print_state st] prints information about the current state of the game [st],
-    for example, the very first time print_state is called, it should show the
-    players in the game, their stacks, and the pot. Later it should include the
-    community cards as well as the user's hole cards. *)
+(** [print_state st] prints information about the current state of the 
+    game [st]. For example, the very first time print_state is called, it should 
+    show the players in the game, their stacks, and the pot. Later it should 
+    include the community cards as well as the user's hole cards. *)
 val print_state : State.t -> unit
 
 (** [play_game num_players] begins a game of poker with a certain amount of
